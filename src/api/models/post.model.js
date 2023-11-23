30794568da99e8ce4010d0f301125a4eaad374f7
UsuarioM <eiroa007@gmail.com>
2023-11-23 09:48:42 +0100

    respaldo hoy jueves 23/11

--- src/api/models/post.model.js
+++ src/api/models/post.model.js
@@ -5,8 +5,8 @@ const PostSchema = new mongoose.Schema(
     imagen: { type: String, required: true, trim: true },
     titulo: { type: String, required: true, trim: true },
     contenido: { type: String, required: true, trim: true },
-    usuario: { type: mongoose.Types.ObjectId, ref: "users" },
-    Categoria: { type: String, enum: [discapacidad.mayores] },
+    usuario: { type: mongoose.Types.ObjectId, ref: "usar" },
+    Categoria: { type: String, enum: ["discapacidad.mayores"] },
     publicado: { type: Boolean, required: true, default: false },
     data: { type: String, trim: true },
   },
@@ -16,6 +16,6 @@ const PostSchema = new mongoose.Schema(
   }
 );
 
-const Post = mongoose.model("users", PostSchema);
+const Post = mongoose.model("usar", PostSchema);
 
 module.exports = Post;
